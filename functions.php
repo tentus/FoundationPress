0de87768c862badeb08c868224fb198b5e8a05d2
tentus <tentus@gmail.com>
2014-10-31 08:33:55 -0400

    Optimize requires into foreach loop

--- functions.php
+++ functions.php
@@ -4,29 +4,16 @@ Author: Ole Fredrik Lie
 URL: http://olefredrik.com
 */
 
-
-// Various clean up functions
-require_once('library/cleanup.php');
-
-// Required for Foundation to work properly
-require_once('library/foundation.php');
-
-// Register all navigation menus
-require_once('library/navigation.php');
-
-// Add menu walker
-require_once('library/menu-walker.php');
-
-// Create widget areas in sidebar and footer
-require_once('library/widget-areas.php');
-
-// Return entry meta information for posts
-require_once('library/entry-meta.php');
-
-// Enqueue scripts
-require_once('library/enqueue-scripts.php');
-
-// Add theme support
-require_once('library/theme-support.php');
-
+foreach( array(
+	'cleanup',			// Various clean up functions
+	'foundation',		// Required for Foundation to work properly
+	'navigation',		// Register all navigation menus
+	'menu-walker',		// Add menu walker
+	'widget-areas',		// Create widget areas in sidebar and footer
+	'entry-meta',		// Return entry meta information for posts
+	'enqueue-scripts',	// Enqueue scripts
+	'theme-support',	// Add theme support
+) as $required_file) {
+	require_once('library/' . $required_file . '.php');
+}
 ?>
