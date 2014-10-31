fd47cef31944a4c0fe8c8a78b638b169463f3430
tentus <tentus@gmail.com>
2014-10-31 08:36:30 -0400

    Add check for content, combine <?php?> tags

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,9 @@
-<aside id="sidebar" class="small-12 large-4 columns">
-	<?php do_action('foundationPress_before_sidebar'); ?>
-	<?php dynamic_sidebar("sidebar-widgets"); ?>
-	<?php do_action('foundationPress_after_sidebar'); ?>
-</aside>
+<?php 
+	if(is_active_sidebar('sidebar-widgets')) {
+		echo '<aside id="sidebar" class="small-12 large-4 columns">';
+		do_action('foundationPress_before_sidebar'); 
+		dynamic_sidebar('sidebar-widgets'); 
+		do_action('foundationPress_after_sidebar'); 
+		echo '</aside>';
+	}
+?>
